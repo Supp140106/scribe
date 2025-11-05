b645edeaf3874a943f20af2df1562e3d660d7d22
Supprit Das <suppritdas@gmail.com>
2025-11-05 16:49:28 +0530

    Change socket URL to production endpoint

--- web/src/socket.js
+++ web/src/socket.js
@@ -1,5 +1,5 @@
 
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001"
+const URL = "https://scribe-neon-two.vercel.app/"
 export const socket = io(URL, { autoConnect: false  });
