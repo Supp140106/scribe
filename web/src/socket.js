267672b5c9d84ce42ec69a23f0fa4a574101d275
Supp140106 <suppritdas@gmail.com>
2025-11-07 07:12:38 +0530

    Bug fixed

--- web/src/socket.js
+++ web/src/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3001/";
+const URL = import.meta.env.VITE_SOCKET_URL; // ✅ pulled from .env
 export const socket = io(URL, { autoConnect: false });
